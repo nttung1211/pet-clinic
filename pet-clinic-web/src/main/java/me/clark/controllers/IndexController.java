cbca364b06a49ebe26c6ea68459059f11a5af649
clark <nttung1211@gmail.com>
2021-03-04 14:24:02 +0700

    add index page. Closes #8.

---
+++ pet-clinic-web/src/main/java/me/clark/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package me.clark.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping({"/", "/index"})
+	public String index() {
+		return "index";
+	}
+}
